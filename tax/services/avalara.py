8059dd90c9d5c167d96d13f5ed354bc701725e5e
Matt Strayer <git@mattstrayer.com>
2020-04-15 23:07:18 -0400

    Avalara Service

---
+++ tax/services/avalara.py
@@ -0,0 +1,20 @@
+import httpx
+from django.conf import settings
+
+
+class AvalaraService:
+
+    BASE_URL = "https://rest.avatax.com/api/v2/taxrates"
+    USER = settings.AVALARA_USER
+    PASSWORD = settings.AVALARA_PASSWORD
+
+
+class TaxRate(AvalaraService):
+    """
+    Interface for the fetching Tax Rates from Avalara
+    """
+
+    def by_postal_code(postalCode: str, country="US"):
+        url = f"{AvalaraService.BASE_URL}/bypostalcode?country={country}&postalCode={postalCode}"
+        response = httpx.get(url, auth=(AvalaraService.USER, AvalaraService.PASSWORD))
+        return getattr(response.json(), "totalRate")
