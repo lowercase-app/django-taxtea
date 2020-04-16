af25410801ecef69e83306423d03aa98a9e5e8c1
Matt Strayer <git@mattstrayer.com>
2020-04-15 23:05:44 -0400

    add checks and settings for Avalara API credentials

--- tax/checks.py
+++ tax/checks.py
@@ -20,3 +20,22 @@ def check_USPS_api_auth(appconfig=None, **kwargs):
         messages.append(checks.Critical(msg, hint=hint, id="tax.C001"))
 
     return messages
+
+
+@checks.register("tax")
+def check_Avalara_api_auth(appconfig=None, **kwargs):
+    """Checks if the user has supplied a Avalara username/password."""
+    from . import settings as tax_settings
+
+    messages = []
+
+    if not tax_settings.AVALARA_USER:
+        msg = "Could not find a Avalara User."
+        hint = "Add AVALARA_USER to your settings."
+        messages.append(checks.Critical(msg, hint=hint, id="tax.C003"))
+    if not tax_settings.AVALARA_PASSWORD:
+        msg = "Could not find a Avalara Password."
+        hint = "Add AVALARA_PASSWORD to your settings."
+        messages.append(checks.Critical(msg, hint=hint, id="tax.C004"))
+
+    return messages
--- tax/settings.py
+++ tax/settings.py
@@ -5,3 +5,10 @@ from django.conf import settings
 ############
 USPS_USER = getattr(settings, "TAX_USPS_USER")
 USPS_PASSWORD = getattr(settings, "TAX_USPS_PASSWORD")
+
+
+###############
+# Avalara Api #
+###############
+AVALARA_USER = getattr(settings, "TAX_AVALARA_USER")
+AVALARA_PASSWORD = getattr(settings, "TAX_AVALARA_PASSWORD")
