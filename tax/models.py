d5dcda6184962fcbe759b5bbceeb145c88de1ae7
Matt Strayer <git@mattstrayer.com>
2020-04-18 20:52:33 -0400

    initial migration, basline models

--- tax/models.py
+++ tax/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 from localflavor.us.models import USStateField
 
-# Create your models here.
-
 
 class State(models.Model):
-    abbreviation = USStateField(blanke=False, null=False)
+    abbreviation = USStateField(blank=False, null=False)
     collects_saas_tax = models.BooleanField(default=False)
 
     def __str__(self):
@@ -16,6 +14,7 @@ class ZipCode(models.Model):
     state = models.ForeignKey(State, on_delete=models.CASCADE)
     code = models.CharField(max_length=9)
     tax_rate = models.DecimalField(blank=True, max_digits=3, decimal_places=2)
+    last_checked = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return f"ZipCode: {self.code}, {self.state}"
