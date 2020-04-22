1612ee0b4acb1939303e8e664b2eebca29c0d0f1
Matt Strayer <git@mattstrayer.com>
2020-04-22 13:34:12 -0400

    Add Setting for INVALIDATE_INTERVAL

--- tax/settings.py
+++ tax/settings.py
@@ -3,7 +3,8 @@ from django.conf import settings
 ############
 # USPS API #
 ############
-USPS_USER = getattr(settings, "TAX_USPS_USER", "***REMOVED***")
+USPS_USER = getattr(settings, "TAX_USPS_USER", None)
+# USPS_PASSWORD = getattr(settings, "TAX_USPS_PASSWORD")
 
 
 ###############
