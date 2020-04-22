4bb3d9bec7c78c616c6aa3aebb6e14598383edad
Matt Strayer <git@mattstrayer.com>
2020-04-21 21:24:50 -0400

    remove USPS password requirement

--- tax/checks.py
+++ tax/checks.py
@@ -12,10 +12,6 @@ def check_USPS_api_auth(appconfig=None, **kwargs):
         msg = "Could not find a USPS User."
         hint = "Add USPS_USER to your settings."
         messages.append(checks.Critical(msg, hint=hint, id="tax.C001"))
-    # if not tax_settings.USPS_PASSWORD:
-    #     msg = "Could not find a USPS Password."
-    #     hint = "Add USPS_PASSWORD to your settings."
-    #     messages.append(checks.Critical(msg, hint=hint, id="tax.C001"))
 
     return messages
 
--- tax/settings.py
+++ tax/settings.py
@@ -4,7 +4,6 @@ from django.conf import settings
 # USPS API #
 ############
 USPS_USER = getattr(settings, "TAX_USPS_USER", "***REMOVED***")
-# USPS_PASSWORD = getattr(settings, "TAX_USPS_PASSWORD")
 
 
 ###############
