7b67b866971e263710e620babe46be45fe4167da
Matt Strayer <git@mattstrayer.com>
2020-04-22 14:24:28 -0400

    fix typing error and point USER to settings user

--- tax/services/usps.py
+++ tax/services/usps.py
@@ -1,4 +1,6 @@
 import xmltodict
+from typing import List
+from tax import settings
 
 # from django.conf import settings
 import httpx
@@ -10,10 +12,8 @@ class USPSService:
     """
 
     BASE_URL = "https://secure.shippingapis.com/ShippingAPI.dll?"
-    # _USER = settings.USPS_USER
-    # _PASSWORD = settings.USPS_PASSWORD
-    USER = ""
-    # _PASSWORD = settings.USPS_PASSWORD
+
+    USER = settings.USPS_USER
 
 
 class ZipService(USPSService):
@@ -24,7 +24,7 @@ class ZipService(USPSService):
     BASE_URL = f"{USPSService.BASE_URL}API=CityStateLookup"
 
     @classmethod
-    def _generate_xml_payload(cls, zipcodes: list[str]):
+    def _generate_xml_payload(cls, zipcodes: List[str]):
         payload = {
             "CityStateLookupRequest": {
                 "@USERID": USPSService.USER,
@@ -34,7 +34,7 @@ class ZipService(USPSService):
         return xmltodict.unparse(payload)
 
     @classmethod
-    def lookup_zips(cls, zipcodes: list[str]):
+    def lookup_zips(cls, zipcodes: List[str]):
         url = f"{ZipService.BASE_URL}&XML={cls._generate_xml_payload(zipcodes)}"
         response = httpx.get(url)
         return (
