9ece644f0ebdd6f786c816777a4ae908c5d547e6
Matt Strayer <git@mattstrayer.com>
2020-04-25 10:41:06 -0400

    cleanup

--- tax/core.py
+++ tax/core.py
@@ -4,12 +4,6 @@ from tax.models import State, ZipCode
 from tax.services.usps import ZipService
 
 
-def connectZipToState(zipCode: str):
-    # Create Zipcode & link to state in DB from USPS
-    res = ZipService.lookup_zips([zipCode])
-    citystate = res.pop()
-    state = State.objects.get(abbreviation=citystate.get("State"))
-    return ZipCode.objects.create(code=citystate.get("Zip5"), state=state)
 
 
 def stateForZip(zipCode: str) -> State:
