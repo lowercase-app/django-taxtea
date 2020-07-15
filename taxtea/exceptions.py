ab4943b6f7b1639a38dd054d2064ab7ca5e2b59d
Matt Strayer <git@mattstrayer.com>
2020-07-15 12:09:03 -0400

    exceptions docstrings

--- taxtea/exceptions.py
+++ taxtea/exceptions.py
@@ -4,16 +4,36 @@ TaxTea Exceptions
 
 
 class USPSError(Exception):
+    """
+    USPSError:
+        Error with the USPS Service
+    """
+
     pass
 
 
 class AvalaraError(Exception):
+    """
+    AvalaraError:
+        Base Error for Avalara Service
+    """
+
     pass
 
 
 class AvalaraRateLimit(AvalaraError):
+    """
+    AvalaraRateLimit:
+        Rate Limit exceeded for Avalara Service
+    """
+
     pass
 
 
 class InvalidZipCode(Exception):
+    """
+    InvalidZipCode:
+        Invalid Zip Code
+    """
+
     pass
