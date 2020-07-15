6897165b4811a1d9cb82d88706305f39d197fe4d
Matt Strayer <git@mattstrayer.com>
2020-07-15 11:15:40 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,16 @@
 # conf.py
 
 # Add napoleon to the extensions list
-extensions = ["sphinx.ext.napoleon"]
+extensions = [
+    "sphinx.ext.napoleon",
+    "sphinx.ext.todo",
+    "sphinx.ext.githubpages",
+]
 
+
+# -- Project information -----------------------------------------------------
+
+project = "Django TaxTea"
+copyright = "2020, lowercase.app"
+author = "Matt Strayer"
+html_theme = "sphinx_rtd_theme"
